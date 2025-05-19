1fc78064122016c0ede753f8d416f5bfa635da15
minseo <3058ms@naver.com>
2025-05-19 18:46:49 +0900

    [김민서] database Service

---
+++ src/main/java/com/minse0/spring/ex/database/UsedGoodsController.java
@@ -0,0 +1,5 @@
+package com.minse0.spring.ex.database;
+
+public class UsedGoodsController {
+
+}
---
+++ src/main/java/com/minse0/spring/ex/database/service/UsedGoodsService.java
@@ -0,0 +1,16 @@
+package com.minse0.spring.ex.database.service;
+
+import org.springframework.stereotype.Service;
+
+//로직 담당
+@Service
+public class UsedGoodsService {
+	
+	// 중고 게시글 모든 벙보를 response에 담는 페이지
+	public  getUsedGoodsList(){
+		
+		// 중고 게시글 정보 얻어 오기
+		UsedGoodsService usedGoodsService = new UsedGoodsService;
+	}
+	
+}
